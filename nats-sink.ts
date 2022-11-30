f39f296469dbd4c52c63f2e47374e21922b08ed7
Unai Perez <unaipme@users.noreply.github.com>
2022-11-30 13:06:12 +0100

    Implemented simple NATS sink

--- nats-sink.ts
+++ nats-sink.ts
@@ -1,29 +1,26 @@
+import { connect, StringCodec } from "nats";
 import * as nodered from "node-red";
 import { NatsNode, NatsSinkNodeDef } from "./nats-def";
 
 module.exports = (RED: nodered.NodeAPI): void => {
   const NatsSinkNode = function (this: NatsNode, config: NatsSinkNodeDef): void {
+    RED.nodes.createNode(this, config);
+
     const _this = this;
-    // _this.on("input", async (msg, send, done) => {
-    //   const { payload } = msg;
-    //   send =
-    //     send ||
-    //     function () {
-    //       _this.send.apply(_this, arguments);
-    //     };
+    const sc = StringCodec();
+    _this.on("input", async (msg, send, done) => {
+      const connection = await connect({
+        servers: [config.server],
+        reconnect: false,
+      });
+      const { payload } = msg;
+      connection.publish(config.topic, sc.encode(payload as string));
+      await connection.drain();
 
-    //   const message: nodered.NodeMessage = {
-    //     ...msg,
-    //     payload: config.isLower
-    //       ? (payload as string).toLowerCase()
-    //       : (payload as string).toUpperCase(),
-    //   };
-    //   _this.send(message);
-
-    //   if (done) {
-    //     done();
-    //   }
-    // });
+      if (done) {
+        done();
+      }
+    });
   };
   RED.nodes.registerType("NATS out", NatsSinkNode);
 };
